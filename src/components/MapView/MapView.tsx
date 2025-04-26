1b816ff25eaf4d50bde76e7ab49edf2402ed3cf9
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2025-04-26 14:14:54 +0200

    chore: supression du warning dans la mpaview

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -1,21 +1,21 @@
 import * as L from "leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
 import { useEffect } from "preact/hooks";
 import "./MapView.css";
-import { warn } from "@tauri-apps/plugin-log";
 
 interface IMapViewProps {
   coordinates: [number, number, number];
 }
 function MapView(props: IMapViewProps) {
   useEffect(() => {
-    warn(JSON.stringify(props.coordinates));
     const center = new L.LatLng(props.coordinates[1], props.coordinates[0]);
     const map = L.map("map").setView(center, 15);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-    L.marker(center).addTo(map);
+    const icon = new L.Icon({ iconUrl: markerIconPng });
+    L.marker(center, { icon }).addTo(map);
   }, [props.coordinates]);
   if (props.coordinates) {
     return <div id="map" className="map-view"></div>;
